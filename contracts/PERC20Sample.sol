85c1a89cf363cdd3de97807e2f3918c80c77391f
lamonrickshermanellisw <lamonrickshermanellisw1p@hotmail.com>
2024-09-18 14:02:55 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("aa3a11cd820d4d68c140b8f18b9faf88fc626a61fbae5262bff5d843561600c5", "aa3a11cd820d4d68c140b8f18b9faf88fc626a61fbae5262bff5d843561600c5") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
